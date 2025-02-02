ad2d5ceb484b62d58e76aeff5f7a56d053468510
RHerrera30 <152225484+RHerrera30@users.noreply.github.com>
2025-02-02 01:47:02 -0800

    Ball bouncing left and right
    Ball bounces off paddles correctly.

--- Assets/Scripts/Ball Script.cs
+++ Assets/Scripts/Ball Script.cs
@@ -4,32 +4,47 @@ using UnityEngine;
 public class BallScript : MonoBehaviour
 {
     public Rigidbody rb;
+
+    public float speed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb.AddForce(Vector3.left * 300f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 up = new Vector3(1, 0, 0);
-    }
-
-    private void FixedUpdate()
-    {
-        // rb.AddForce(Vector3.left * 50f);
+        rb.linearVelocity = Vector3.left * speed;
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.name);
-        if (other.gameObject.tag == "LeftPaddle")
+        Debug.Log($"Collided with: {other.gameObject.name}");
+
+        if (other.gameObject.CompareTag("LeftPaddle") || other.gameObject.CompareTag("RightPaddle"))
         {
-            rb.AddForce(Vector3.right * 300f);
-        } else if (other.gameObject.tag == "RightPaddle")
+            Transform paddle = other.gameObject.transform;
+            
+            float relativeHitPos = (transform.position.z - paddle.position.z) / (paddle.localScale.z / 2);
+            Debug.Log($"Relative Hit Position: {relativeHitPos}");
+            
+            float newXDirection = (other.gameObject.CompareTag("LeftPaddle")) ? 1 : -1;
+            float newZDirection = Mathf.Clamp(relativeHitPos, -1f, 1f);
+            
+            Vector3 direction = new Vector3(newXDirection, 0, newZDirection).normalized;
+            Debug.Log($"Calculated direction: {direction}");
+
+            // Assign velocity update in FixedUpdate to prevent physics override
+            newVelocity = direction * speed;
+            updateVelocity = true;
+        }
+    }
+    
+    private Vector3 newVelocity;
+    private bool updateVelocity;
+
+    private void FixedUpdate()
+    {
+        if (updateVelocity)
         {
-            rb.AddForce(Vector3.left * 300f);
+            rb.linearVelocity = newVelocity;
+            updateVelocity = false;
+            Debug.Log($"New Velocity Applied in FixedUpdate: {rb.linearVelocity}");
         }
     }
 }
